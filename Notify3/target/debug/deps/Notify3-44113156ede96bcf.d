1b859ae0dfda27a4d402f6978b6271633b9d9675
Zanzibarr <matteozanella2@gmail.com>
2024-04-24 16:52:20 +0200

    Start to work on Rust

--- Notify3/target/debug/deps/Notify3-44113156ede96bcf.d
+++
@@ -1,5 +0,0 @@
-/home/zanzi/shared/Notify2/Notify3/target/debug/deps/libNotify3-44113156ede96bcf.rmeta: src/main.rs
-
-/home/zanzi/shared/Notify2/Notify3/target/debug/deps/Notify3-44113156ede96bcf.d: src/main.rs
-
-src/main.rs:
---
+++ notify3/src/utils.rs
@@ -0,0 +1,3 @@
+fn prntinfo(msg : str, loc : str = "", line : i32 = "", sugg : str = "") -> None {
+    println!(msg);
+}
