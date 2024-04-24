73594b6531cfc9346eda8fd1938b3eae00d4de9e
Zanzibarr <matteozanella2@gmail.com>
2024-04-24 16:24:59 +0200

    Start to work on Rust

---
+++ Notify3/target/debug/deps/Notify3-44113156ede96bcf.d
@@ -0,0 +1,5 @@
+/home/zanzi/shared/Notify2/Notify3/target/debug/deps/libNotify3-44113156ede96bcf.rmeta: src/main.rs
+
+/home/zanzi/shared/Notify2/Notify3/target/debug/deps/Notify3-44113156ede96bcf.d: src/main.rs
+
+src/main.rs:
---
+++ Notify3/target/debug/deps/Notify3-ffe1cd893ceb2c2f.d
@@ -0,0 +1,5 @@
+/home/zanzi/shared/Notify2/Notify3/target/debug/deps/libNotify3-ffe1cd893ceb2c2f.rmeta: src/main.rs
+
+/home/zanzi/shared/Notify2/Notify3/target/debug/deps/Notify3-ffe1cd893ceb2c2f.d: src/main.rs
+
+src/main.rs:
