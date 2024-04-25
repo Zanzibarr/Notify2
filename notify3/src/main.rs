07c036b0335bba48c67a4d8956dda27f9cff4df2
Zanzibarr <matteozanella2@gmail.com>
2024-04-25 23:01:07 +0200

    Notify3:utils:logging

--- notify3/src/main.rs
+++ notify3/src/main.rs
@@ -1,3 +1,11 @@
+mod utils;
+
 fn main() {
-    println!("Ciao");
+    utils::info("Hello, I have an important question for you.");
+    let choice = utils::input("Do you like icecream?", &["y", "n"]);
+    match choice.as_str() {
+        "y" => utils::info("Really?!?!"),
+        _ => utils::error("We can't be friends..."),
+    }
+    utils::info("I like it too!!!");
 }
--- notify3/src/utils.rs
+++ notify3/src/utils.rs
@@ -1,3 +1,42 @@
-fn prntinfo(msg : str, loc : str = "", line : i32 = "", sugg : str = "") -> None {
-    println!(msg);
+use std::{io::{self, Write}, process::exit};
+
+static INFO  : &str = "\x1b[92m\x1b[1m[ INFO  ]:\x1b[0m ";
+static INPUT : &str = "\x1b[94m\x1b[1m[ INPUT ]:\x1b[0m ";
+static WARN  : &str = "\x1b[93m\x1b[1m[ WARN  ]:\x1b[0m ";
+static ERROR : &str = "\x1b[91m\x1b[1m[ ERROR ]:\x1b[0m ";
+
+pub fn info<T:std::fmt::Display>(msg : T) {
+    println!("{INFO}{msg}");
+}
+
+pub fn warn<T:std::fmt::Display>(msg: T) {
+    println!("{WARN}{msg}");
+}
+
+pub fn error<T:std::fmt::Display>(msg: T) {
+    println!("{ERROR}{msg}");
+    exit(1);
+}
+
+pub fn input<T:std::fmt::Display>(msg : T, choices : &[&str]) -> String {
+
+    loop {
+
+        print!("{INPUT}{msg} {choices:?} : ");
+        io::stdout().flush()
+            .expect("Error flushing stdout");
+    
+        let mut choice = String::new();
+        io::stdin().read_line(&mut choice)
+            .expect("Errror reading input");
+    
+        choice = choice.trim().to_string();
+
+        match choices.contains(&choice.as_str()) {
+            true => return choice,
+            false => warn("Invalid input."),
+        };
+
+    }
+
 }
