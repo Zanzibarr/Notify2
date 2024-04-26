32fcb38faabdaf70cbacee62992e16d81c8091ee
Zanzibarr <matteozanella2@gmail.com>
2024-04-26 15:27:00 +0200

    Notify3:utils:API post/get methods(unfinished)

--- notify3/src/main.rs
+++ notify3/src/main.rs
@@ -1,11 +1,13 @@
 mod utils;
 
 fn main() {
-    utils::info("Hello, I have an important question for you.");
-    let choice = utils::input("Do you like icecream?", &["y", "n"]);
-    match choice.as_str() {
-        "y" => utils::info("Really?!?!"),
-        _ => utils::error("We can't be friends..."),
-    }
-    utils::info("I like it too!!!");
+
+  let command = "https://www.zanzi.dev";
+
+  match utils::reqget(command) {
+    Ok(Some(status)) => utils::info(status),
+    Ok(None) => utils::warn("No response"),
+    Err(err) => utils::warn(err),
+  };
+
 }
--- notify3/src/utils.rs
+++ notify3/src/utils.rs
@@ -1,4 +1,31 @@
+#![allow(dead_code)]
+
 use std::{io::{self, Write}, process::exit};
+
+// API
+
+#[tokio::main]
+pub async fn reqget<T:std::fmt::Display>(url : T) -> Result<Option<reqwest::StatusCode>, reqwest::Error> {
+
+    let client = reqwest::Client::new();
+    let response = client.get(format!("{url}")).send().await?;
+    let status_code = response.status();
+    Ok(Some(status_code))
+
+}
+
+#[tokio::main]
+pub async fn reqpost<T:std::fmt::Display>(url: T) -> Result<Option<reqwest::StatusCode>, reqwest::Error> {
+
+    let client = reqwest::Client::new();
+    let response = client.post(format!("{url}")).send().await?;
+    todo!("Finish: see notify.py __request_format");
+    let status_code = response.status();
+    Ok(Some(status_code))
+
+}
+
+// LOGGING
 
 static INFO  : &str = "\x1b[92m\x1b[1m[ INFO  ]:\x1b[0m ";
 static INPUT : &str = "\x1b[94m\x1b[1m[ INPUT ]:\x1b[0m ";
@@ -6,19 +33,22 @@ static WARN  : &str = "\x1b[93m\x1b[1m[ WARN  ]:\x1b[0m ";
 static ERROR : &str = "\x1b[91m\x1b[1m[ ERROR ]:\x1b[0m ";
 
 pub fn info<T:std::fmt::Display>(msg : T) {
-    println!("{INFO}{msg}");
+    eprintln!("{INFO}{msg}");
 }
 
 pub fn warn<T:std::fmt::Display>(msg: T) {
-    println!("{WARN}{msg}");
+    eprintln!("{WARN}{msg}");
 }
 
 pub fn error<T:std::fmt::Display>(msg: T) {
-    println!("{ERROR}{msg}");
+    eprintln!("{ERROR}{msg}");
     exit(1);
 }
 
-pub fn input<T:std::fmt::Display>(msg : T, choices : &[&str]) -> String {
+pub fn input<T:std::fmt::Display>(
+    msg : T,
+    choices : &[&str]
+) -> String {
 
     loop {
 
